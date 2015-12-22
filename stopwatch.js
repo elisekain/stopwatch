dd8fe728575d4a3596c1f0114b9130b3b527c76c
Elise Kain <elise.kain@gmail.com>
2015-12-22 16:54:53 -0500

    Initial Commit

---
+++ stopwatch.js
@@ -0,0 +1,14 @@
+// Timer tracks minutes, seconds, hundreds.
+// Switches to hour, minutes, second after 60 min.
+// Start button
+  // Switches to Stop button when selected
+  // Switches back to start when selected
+// Reset button
+  // Switches to Lap button when timer starts
+  // Lap button - when clicked documents the lap time
+  // Switches back to Reset when timer stops
+
+// UI
+  // Timer
+  // Lap Timer (above timer & smaller)
+  // Laps are documented & tracked. Latest lap is at the top.
