2c2337ba9a0232edc5c06b535b26a8b496fd6dce
Elise Kain <elise.kain@gmail.com>
2015-12-22 21:37:12 -0500

    Set up timer

--- stopwatch.js
+++ stopwatch.js
@@ -1,6 +1,57 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-});
+  var timer = document.getElementById("timer");
+  var rightButton = document.getElementById("right");
+  var leftButton = document.getElementById("left");
+  var timerID = null;
+  var start;
+
+  // Add prefix "0" if less than 10
+  var addZero = function(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  // Update Time Function
+  var updateTime = function () {
+    timerID = setInterval(function() {
+      var time = new Date();
+      timeElapsed = Math.floor((time - start) / 10);
+      console.log(timeElapsed.type);
+      var minutes = addZero();
+      var seconds = addZero();
+      var tenths = addZero();
+      timer.innerHTML = "<h2>" + timeElapsed + "</h2>";
+    }, 10);
+  }
+
+  var switchText = function(DOMelement, text) {
+    DOMelement.innerHTML = text;
+  }
+
+  // Start Button Event
+  rightButton.addEventListener("click", function(){
+    if (!timerID) {
+      start = Date.now();
+      updateTime();
+      switchText(rightButton, "Stop");
+    } else if (timerID) {
+      clearInterval(timerID);
+      timerID = null;
+      switchText(rightButton, "Start");
+    }
+  });
+
+  leftButton.addEventListener("click", function(){
+  });
+
+
+}, false);
+
+
+
 // Timer tracks minutes, seconds, hundreds.
 // Switches to hour, minutes, second after 60 min.
 // Start button
