cbdc7b2e980015752200da2611fd0b61c4dd3eb1
Elise Kain <elise.kain@gmail.com>
2015-12-23 12:35:22 -0500

    Refactor event listener functions

--- stopwatch.js
+++ stopwatch.js
@@ -6,10 +6,9 @@ document.addEventListener('DOMContentLoaded', function(){
   var leftButton = document.getElementById("left");
   var rightButton = document.getElementById("right");
   var timerID;
-  var startTime;
   var savedTime = 0;
-  var lapNumber = 1;
   var lapTimeStart = 0;
+  var lapNumber = 1;
 
   // Add prefix "0" if less than 10
   var addZeroes = function(value) {
@@ -24,10 +23,12 @@ document.addEventListener('DOMContentLoaded', function(){
     var minutes = Math.floor((inputTime / 1000 / 60) % 60);
     var seconds = Math.floor((inputTime / 1000) % 60);
     var hundreths = Math.floor((inputTime / 10) % 100);
+
     hours = addZeroes(hours);
     minutes = addZeroes(minutes);
     seconds = addZeroes(seconds);
     hundreths = addZeroes(hundreths);
+
     if (hours > 1) {
       var html = hours + ":" + minutes + ":" + seconds;
     } else {
@@ -40,15 +41,12 @@ document.addEventListener('DOMContentLoaded', function(){
   var updateTime = function (prevSavedTime) {
     timerID = setInterval(function() {
       // Main Timer
-      var timeNow = new Date();
-      newTimeElapsed = timeNow - startTime;
-      savedTime = newTimeElapsed + prevSavedTime;
+      savedTime = (new Date() - startTime) + prevSavedTime;
       timer.innerHTML = "<h2>" + formatTime(savedTime) + "</h2>";
 
       // Lap Timer
       var lapTime = savedTime - lapTimeStart;
       lapTimer.innerHTML = "<p>" + formatTime(lapTime) + "</p>";
-
     }, 10);
   }
 
@@ -56,53 +54,53 @@ document.addEventListener('DOMContentLoaded', function(){
     DOMelement.innerHTML = text;
   }
 
+  var startTimer = function() {
+    startTime = Date.now();
+    updateTime(savedTime);
+    switchText(rightButton, "Stop");
+    switchText(leftButton, "Lap");
+  }
+
+  var stopTimer = function() {
+    clearInterval(timerID);
+    timerID = null;
+    startTime = null;
+    switchText(rightButton, "Start");
+    switchText(leftButton, "Reset");
+  }
+
+  var resetTimer = function() {
+    savedTime = 0;
+    lapNumber = 1;
+    lapTimeStart = 0;
+    timer.innerHTML = "<h2>00:00:00</h2>";
+    lapTimer.innerHTML = "<p>00:00:00</p>";
+    lapTable.innerHTML = "";
+  }
+
+  var saveLap = function() {
+    var saveLap = savedTime - lapTimeStart;
+    var newLapDisplay = document.createElement("p");
+    newLapDisplay.innerHTML = "Lap " + lapNumber + " " + formatTime(saveLap);
+    lapTable.insertBefore(newLapDisplay, lapTable.childNodes[0]);
+    lapTimeStart = savedTime;
+    lapNumber++;
+  }
+
+  // Event Listeners
   rightButton.addEventListener("click", function(){
     if (!timerID) {
-      startTime = Date.now();
-      updateTime(savedTime);
-      switchText(rightButton, "Stop");
-      switchText(leftButton, "Lap");
-    } else if (timerID) {
-      clearInterval(timerID);
-      timerID = null;
-      startTime = null;
-      switchText(rightButton, "Start");
-      switchText(leftButton, "Reset");
+      startTimer();
+    } else {
+      stopTimer();
     }
   });
 
   leftButton.addEventListener("click", function(){
     if (!timerID) {
-      timerID = null;
-      startTime = null;
-      savedTime = 0;
-      lapNumber = 1;
-      lapTimeStart = 0;
-      timer.innerHTML = "<h2>00:00:00</h2>";
-      lapTimer.innerHTML = "<p>00:00:00</p>";
-      lapTable.innerHTML = "";
+      resetTimer();
     } else {
-      var saveLap = savedTime - lapTimeStart;
-      var newLapDisplay = document.createElement("p");
-      newLapDisplay.innerHTML = "Lap " + lapNumber + " " + formatTime(saveLap);
-      lapTable.insertBefore(newLapDisplay, lapTable.childNodes[0]);
-      lapTimeStart = savedTime;
-      lapNumber++;
+      saveLap();
     }
   });
 }, false);
-
-
-// Switches to hour, minutes, second after 60 min.
-// Start button
-  // Switches to Stop button when selected
-  // Switches back to start when selected
-// Reset button
-  // Switches to Lap button when timer starts
-  // Lap button - when clicked documents the lap time
-  // Switches back to Reset when timer stops
-
-// UI
-  // Timer
-  // Lap Timer (above timer & smaller)
-  // Laps are documented & tracked. Latest lap is at the top.
